5f4099c85bc2ca6049ae40562e4be1f2d77d0630
USER <USER@DESKTOP-FRBRUEK>
2021-11-27 21:42:54 +0300

    Добавьте файлы проекта.

---
+++ Task5.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            Console.Write("Введите семь элементов массива: \t ");
+            int elementsCount = int.Parse(Console.ReadLine());
+            int[]myArray = new int[elementsCount];  
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}:\t");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            int avg, Sum ;
+            avg=0;
+            Sum=0;
+           
+                for (int i = 0; i< elementsCount; i++)
+            {
+                Sum += myArray [i];
+                avg= Sum/ myArray.Length;
+            }
+                Console.Write(" Среднее арифметическое равно = {0} ", avg);
+ 
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
